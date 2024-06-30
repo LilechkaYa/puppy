21429b4fccfae79a006e00685e5b91be44f916eb
Lilita <lilita.nikushkina@gmail.com>
2024-06-30 20:33:28 +0100

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/LilechkaYa/puppy
 
 go 1.21.4
+
+require github.com/LilechkaYa/dog v0.0.0-20240630184423-8f88bdd2be9c // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/LilechkaYa/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woooof!!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
